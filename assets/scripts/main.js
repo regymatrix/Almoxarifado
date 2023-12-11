5bc7f4e1537cc7cb44e23ac0638967e960b328d7
Amanda de Jesus Melo <60000299+amandexspeed@users.noreply.github.com>
2023-12-11 14:26:38 -0300

    modificação de funções

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,4 +1,4 @@
-document.getElementById('').addEventListener('focus',function(){
+/* document.getElementById('').addEventListener('focus',function(){
 
   const inputDepartamento = document.getElementById('departamento');
   inputDepartamento.style.backgroundColor="green";
@@ -8,4 +8,15 @@ document.getElementById('').addEventListener('blur',function(){
 
   const inputDepartamento = document.getElementById('departamento');
   inputDepartamento.style.backgroundColor="white";
-});
+}); */
+
+function adicionarCoraoFocarInput(){
+
+  const listInput = document.querySelectorAll("input[type=text]");
+  console.log(listInput.length);
+  console.log(listInput);
+
+  listInput[1].style.backgroundColor = "green"
+
+}
+
