7aa1c9f9a44de2828db1cd32d7d7fa5899d50f24
Amanda de Jesus Melo <60000299+amandexspeed@users.noreply.github.com>
2023-12-11 14:10:23 -0300

    Update main.js
    Mudança de cor do input

---
+++ assets/scripts/main.js
@@ -0,0 +1,11 @@
+document.getElementById('').addEventListener('focus',function(){
+
+  const inputDepartamento = document.getElementById('departamento');
+  inputDepartamento.style.backgroundColor="green";
+});
+
+document.getElementById('').addEventListener('blur',function(){
+
+  const inputDepartamento = document.getElementById('departamento');
+  inputDepartamento.style.backgroundColor="white";
+});
